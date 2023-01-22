5efdb8b3653a098ea042ba62f016801772e17975
Joshua Lumanog <lumanog.joshua05@gmail.com>
2023-01-22 10:29:26 +0800

    Added an sql query to fill in the profile_key of profile_data table before the account_table to match both table's primay and foreign key

--- server.js
+++ server.js
@@ -5,12 +5,20 @@ const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const bcrypt = require('bcrypt');
 
+const { v4: uuidv4, v4 } = require('uuid');
+const v4options = {
+    random: [
+      0x10, 0x91, 0x56, 0xbe, 0xc4, 0xfb, 0xc1, 0xea, 0x71, 0xb4, 0xef, 0xe1, 0x67, 0x1c, 0x58, 0x36,
+    ],
+}
+
 const app = express();
 
 const connection = require('./app/config/db.config');
 const corsConfiguration = require('./app/config/cors.config');
 
 const PORT = process.env.PORT || 8080;
+
 
 
 
@@ -25,8 +33,8 @@ app.use(session({
         resave: false,
         saveUninitialized: false,
         cookie: {
-            maxAge: 300000,
-            expires: 300000
+            maxAge: 900000,
+            expires: 900000
         }
     })
 );
@@ -75,6 +83,7 @@ app.post('/login', (req, res)=>{
 })
 
 app.post('/register', (req, res)=>{
+    let profile_key = uuidv4(v4options);
     let saltRounds = 10;
     console.log(req.body.password);
     bcrypt.hash(req.body.password, saltRounds, (err, hash)=>{
@@ -83,7 +92,11 @@ app.post('/register', (req, res)=>{
         }
         else{
             console.log(hash);
-            connection.query(`INSERT INTO account_table (username, password) VALUES ('${req.body.username}', '${hash}');`);
+            //REMEBER - When dealing with foreign keys, the primary key cell of the second table should have a value first before the foreign key cell of the first error.
+            //Otherwise, it wont work.
+            connection.query(`INSERT INTO profile_data (profile_key) VALUES ('${profile_key}')`);
+            connection.query(`INSERT INTO account_table (username, password, profile_key) VALUES ('${req.body.username}', '${hash}', '${profile_key}');`);
+
             return res.send("ACCOUNT REGISTERED SUCCESSFULLY");
         }
     })
